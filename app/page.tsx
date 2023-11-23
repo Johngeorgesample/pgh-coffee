adff9bf828477c2a4c4cf993fa627d7d86edd9df
John-George Sample <johngeorgesample@gmail.com>
2023-11-22 21:37:07 -0500

    styles

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <div className="flex flex-col relative flex place-items-center">
-        <h1> pgh.coffee </h1>
-        <h3>A guide to every coffee shop in and around Pittsburgh, PA</h3>
+        <h1 className="text-4xl pb-6"> pgh.coffee </h1>
+        <h3 className="">A guide to every coffee shop in and around Pittsburgh, PA</h3>
       </div>
     </main>
   )
