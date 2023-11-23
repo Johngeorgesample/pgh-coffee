495996000a5d1a0716c238e8d0da22e95fb5b75b
John-George Sample <johngeorgesample@gmail.com>
2023-11-23 10:04:42 -0500

    center subtitle

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center">
       <div className="flex flex-col relative flex place-items-center">
         <h1 className="text-4xl pb-6"> pgh.coffee </h1>
-        <h3 className="">A guide to every coffee shop in and around Pittsburgh, PA</h3>
+        <h3 className="text-center">A guide to every coffee shop in and around Pittsburgh, PA</h3>
       </div>
     </main>
   )
