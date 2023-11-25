37681081a3b6e08ea538fdc35b9cdcfa8f7467ae
John-George Sample <johngeorgesample@gmail.com>
2023-11-25 18:54:24 -0500

    add link to card

--- components/ShopCard.tsx
+++ components/ShopCard.tsx
@@ -11,10 +11,12 @@ type TShop = {
 
 export default function ShopCard(props: IProps) {
   return (
-    <div className="w-56 h-56 border">
-      <p>{props.shop.name}</p>
-      <p>{props.shop.neighborhood}</p>
-      <p>{props.shop.address}</p>
-    </div>
+    <a className="" href={props.shop.website} target="_blank">
+      <div className="h-36 p-2 border hover:border-gray-600">
+        <p>{props.shop.name}</p>
+        <p>{props.shop.neighborhood}</p>
+        <address>{props.shop.address}</address>
+      </div>
+    </a>
   )
 }
