1aec87ee31ce19ffa71154216ce3108b9e5b54c3
John-George Sample <johngeorgesample@gmail.com>
2023-11-26 18:45:47 -0500

    use the most basic tailwind styles possible

--- app/components/ShopCard.tsx
+++ app/components/ShopCard.tsx
@@ -11,12 +11,14 @@ type TShop = {
 
 export default function ShopCard(props: IProps) {
   return (
-    <a className="" href={props.shop.website} target="_blank">
-      <div className="h-36 p-2 border hover:border-gray-600">
-        <p>{props.shop.name}</p>
-        <p>{props.shop.neighborhood}</p>
-        <address>{props.shop.address}</address>
-      </div>
-    </a>
+    <div className="rounded overflow-hidden shadow-md hover:bg-gray-100">
+      <a className="" href={props.shop.website} target="_blank">
+        <div className="h-36 px-6 py-4">
+          <p className="font-bold text-xl mb-1">{props.shop.name}</p>
+          <p className="text-gray-700">{props.shop.neighborhood}</p>
+          <address className="text-gray-700">{props.shop.address}</address>
+        </div>
+      </a>
+  </div>
   )
 }
