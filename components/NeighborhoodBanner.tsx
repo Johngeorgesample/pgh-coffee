ecf851b572ff6ddf5038ec3531473764817d3199
John-George Sample <johngeorgesample@gmail.com>
2023-11-25 19:00:51 -0500

    create NeighborhoodBanner component

---
+++ components/NeighborhoodBanner.tsx
@@ -0,0 +1,11 @@
+interface IProps {
+  neighborhood: string
+}
+
+export default function NeighborhoodBanner(props: IProps) {
+  return (
+    <div className="w-full bg-yellow-200 px-4 py-2 my-4">
+      {props.neighborhood}
+    </div>
+  )
+}
