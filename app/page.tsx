e22f07b90a7c40d5bbe3d3474e6ee4f00949b048
John-George Sample <johngeorgesample@gmail.com>
2023-11-26 18:36:58 -0500

    let's start small

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center">
       <div className="flex flex-col relative flex place-items-center">
         <h1 className="text-4xl pb-6"> pgh.coffee </h1>
-        <h3 className="text-center">A guide to every coffee shop in and around Pittsburgh, PA</h3>
+        <h3 className="text-center">A guide to every coffee shop in Pittsburgh, PA</h3>
         <hr className="my-4 w-1/2 m-auto" />
 
         <div className="grid grid-cols-3 gap-4 px-4">
