9a225ebd52e85f7c53b22dc4b63f6fc320b7d3f2
John-George Sample <johngeorgesample@gmail.com>
2023-11-26 18:45:34 -0500

    use Footer component

---
+++ app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="mt-4 p-4 w-full bg-gray-200">
+      <p className="text-center">Made with ❤️ and ☕ by <a className="underline" href="https://www.johngeorgesample.com/" target="_blank">John-George Sample</a></p>
+    </footer>
+  );
+}
