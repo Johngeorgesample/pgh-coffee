dd892564ea22a930c95cdcd4f1e782d79c24497c
John-George Sample <johngeorgesample@gmail.com>
2023-11-27 07:26:54 -0500

    remove unused styles, close div sooner

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,11 @@ import Footer from '@/app/components/Footer'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <div className="flex flex-col relative flex place-items-center">
+      <div className="flex flex-col items-center mt-2">
         <h1 className="text-4xl pb-6"> pgh.coffee </h1>
         <h3 className="text-center">A guide to every coffee shop in Pittsburgh, PA</h3>
         <hr className="my-4 w-1/2 m-auto" />
+      </div>
 
         <div className="grid grid-cols-3 gap-4 px-4">
           {CoffeeShops.map((shop) => {
@@ -18,7 +19,6 @@ export default function Home() {
         </div>
 
           <Footer />
-      </div>
     </main>
   )
 }
