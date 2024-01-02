68f40856c651b0fb85e7f4c39b59f1fad0ee3de5
John-George Sample <johngeorgesample@gmail.com>
2024-01-02 08:44:07 -0500

    styles for hovering neighborhood, start filtering function

--- app/components/ShopCard.tsx
+++ app/components/ShopCard.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 interface IProps {
   shop: TShop
 }
@@ -10,15 +12,16 @@ type TShop = {
 }
 
 export default function ShopCard(props: IProps) {
+  const filterByNeighborhood = () => {
+    console.log(props.shop.neighborhood);
+  }
   return (
     <div className="group rounded overflow-hidden shadow-md hover:bg-gray-100">
-      <a className="" href={props.shop.website} target="_blank">
         <div className="px-6 py-4">
           <p className="font-bold text-xl mb-1 group-hover:underline">{props.shop.name}</p>
-          <p className="text-gray-700">{props.shop.neighborhood}</p>
+          <p className="w-fit text-gray-700 border border-transparent  hover:border-black hover:border-dashed hover:cursor-pointer">{props.shop.neighborhood}</p>
           <address className="text-gray-700">{props.shop.address}</address>
         </div>
-      </a>
   </div>
   )
 }
