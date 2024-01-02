fd6ddd16ee7bb87cc7c79c256f9036774d467ffd
John-George Sample <johngeorgesample@gmail.com>
2024-01-02 16:27:18 -0500

    start naive approach for filtering by neighborhood

--- app/components/ShopCard.tsx
+++ app/components/ShopCard.tsx
@@ -1,25 +1,23 @@
 'use client'
 
 interface IProps {
+  onShopClick: Function
   shop: TShop
 }
 
 type TShop = {
   name: string,
-  neighborhood: string,
+  neighborhood: string, // @TODO should this be a union type?
   address: string,
-  website: string,
+  website: string, // @TODO how can I verify a URL is valid? Is that a fool's errand?
 }
 
 export default function ShopCard(props: IProps) {
-  const filterByNeighborhood = () => {
-    console.log(props.shop.neighborhood);
-  }
   return (
     <div className="group rounded overflow-hidden shadow-md hover:bg-gray-100">
         <div className="px-6 py-4">
           <p className="font-bold text-xl mb-1 group-hover:underline">{props.shop.name}</p>
-          <p className="w-fit text-gray-700 border border-transparent  hover:border-black hover:border-dashed hover:cursor-pointer">{props.shop.neighborhood}</p>
+          <p className="w-fit text-gray-700 border border-transparent  hover:border-black hover:border-dashed hover:cursor-pointer" onClick={() => props.onShopClick(props.shop.neighborhood)}>{props.shop.neighborhood}</p>
           <address className="text-gray-700">{props.shop.address}</address>
         </div>
   </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,31 @@
+'use client'
+
+import { useState } from 'react'
 import ShopCard from '@/app/components/ShopCard'
 import CoffeeShops from '@/data/coffee_shops.json'
 import Footer from '@/app/components/Footer'
 
 export default function Home() {
+  const [neighborhoodFilter, setNeighborhoodFilter] = useState(null)
+
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gray-50">
         <div className="flex flex-col items-center mt-2">
-          <h1 className="text-4xl pb-6"> pgh.coffee </h1>
+          <h1 className="text-4xl pb-6">pgh.coffee</h1>
           <h3 className="text-center">A guide to every coffee shop in Pittsburgh, PA</h3>
           <hr className="my-4 w-1/2 m-auto" />
         </div>
 
+        { neighborhoodFilter && <button onClick={() => setNeighborhoodFilter(null)}>Clear filter</button> }
         <div className="grid grid-cols-3 gap-4 px-4">
           {CoffeeShops.map((shop) => {
-            return <ShopCard key={shop.address} shop={shop} />
+            if (neighborhoodFilter && shop.neighborhood === neighborhoodFilter) {
+              return <ShopCard key={shop.address} shop={shop} onShopClick={(shopName: any) => setNeighborhoodFilter(shopName)}/>
+            }
+            if (!neighborhoodFilter) {
+              return <ShopCard key={shop.address} shop={shop} onShopClick={(shopName: any) => setNeighborhoodFilter(shopName)}/>
+            }
           }
           )}
         </div>
