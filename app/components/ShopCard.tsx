2bde5aa1b6d2b9d5a60b398da5ec0352e563ee39
John-George Sample <johngeorgesample@gmail.com>
2024-01-08 16:19:14 -0500

    small css tweaks to neighborhood button

--- app/components/ShopCard.tsx
+++ app/components/ShopCard.tsx
@@ -17,7 +17,7 @@ export default function ShopCard(props: IProps) {
     <div className="relative rounded overflow-hidden shadow-md">
         <div className="px-6 py-4">
           <a className="font-bold text-xl mb-1 block hover:underline" href={props.shop.website} target="_blank">{props.shop.name}</a>
-          <button className="w-fit text-gray-700 border border-transparent hover:border-black hover:border-dashed hover:cursor-pointer" onClick={() => props.onShopClick(props.shop.neighborhood)}>{props.shop.neighborhood}</button>
+          <button className="w-fit mb-1 text-left text-gray-700 border border-transparent hover:border-black hover:border-dashed hover:cursor-pointer" onClick={() => props.onShopClick(props.shop.neighborhood)}>{props.shop.neighborhood}</button>
           <address className="text-gray-700">{props.shop.address}</address>
         </div>
   </div>
