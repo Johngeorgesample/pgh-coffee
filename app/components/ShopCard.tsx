2a7e15c625bf93e1f67be3f88698d43554cc2ba1
John-George Sample <johngeorgesample@gmail.com>
2024-01-07 19:27:07 -0500

    Entire card is no longer link, just the shop name

--- app/components/ShopCard.tsx
+++ app/components/ShopCard.tsx
@@ -14,14 +14,12 @@ type TShop = {
 
 export default function ShopCard(props: IProps) {
   return (
-    <div className="group relative rounded overflow-hidden shadow-md hover:bg-gray-100">
-      <button className="w-fit absolute top-12 left-6 text-gray-700 border border-transparent hover:border-black hover:border-dashed hover:cursor-pointer" onClick={() => props.onShopClick(props.shop.neighborhood)}>{props.shop.neighborhood}</button>
-      <a className="" href={props.shop.website} target="_blank">
+    <div className="relative rounded overflow-hidden shadow-md">
         <div className="px-6 py-4">
-          <p className="font-bold text-xl mb-1 group-hover:underline">{props.shop.name}</p>
-          <address className="mt-8 text-gray-700">{props.shop.address}</address>
+          <a className="font-bold text-xl mb-1 block hover:underline" href={props.shop.website} target="_blank">{props.shop.name}</a>
+          <button className="w-fit text-gray-700 border border-transparent hover:border-black hover:border-dashed hover:cursor-pointer" onClick={() => props.onShopClick(props.shop.neighborhood)}>{props.shop.neighborhood}</button>
+          <address className="text-gray-700">{props.shop.address}</address>
         </div>
-      </a>
   </div>
   )
 }
