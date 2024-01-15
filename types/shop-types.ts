b93c577e6e32f16c7b27bffcc7e2f07da443f69a
John-George Sample <johngeorgesample@gmail.com>
2024-01-15 18:34:55 -0500

    Make and use TNeighborhood

--- types/shop-types.ts
+++ types/shop-types.ts
@@ -1,6 +1,8 @@
+import { TNeighborhood } from "./neighborhood-types";
+
 export interface TShop {
   name: string,
-  neighborhood: string, // @TODO should this be a union type?
+  neighborhood: TNeighborhood,
   address: string,
   website: string, // @TODO how can I verify a URL is valid? Is that a fool's errand?
   value: any,
