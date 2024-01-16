e8162bc2c1078bc7fa4e9c960dbd764103925318
John-George Sample <johngeorgesample@gmail.com>
2024-01-15 19:05:57 -0500

    update comment

--- types/shop-types.ts
+++ types/shop-types.ts
@@ -4,6 +4,6 @@ export interface TShop {
   name: string,
   neighborhood: TNeighborhood,
   address: string,
-  website: string, // @TODO how can I verify a URL is valid? Is that a fool's errand?
+  website: string, // @TODO make `URL` work
   value?: any,
 }
