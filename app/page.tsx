b57222f33e88f68301a11c62de05e49439882d36
John-George Sample <johngeorgesample@gmail.com>
2024-01-15 19:23:26 -0500

    add comment

--- app/page.tsx
+++ app/page.tsx
@@ -58,7 +58,6 @@ export default function Home() {
             }
           })}
         </div>
-
       </main>
     <Footer />
     </>
--- types/shop-types.ts
+++ types/shop-types.ts
@@ -2,7 +2,7 @@ import { TNeighborhood } from "./neighborhood-types";
 
 export interface TShop {
   name: string,
-  neighborhood: TNeighborhood,
+  neighborhood: string, // @TODO make `TNeighborhood` work
   address: string,
   website: string, // @TODO make `URL` work
   value?: any,
