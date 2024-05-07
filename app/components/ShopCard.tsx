8ba6ace57f8d3ff02eecd8770d2fab1025404be8
John-George Sample <johngeorgesample@gmail.com>
2024-05-07 11:26:41 -0400

    tweak font-weight

--- app/components/ShopCard.tsx
+++ app/components/ShopCard.tsx
@@ -20,7 +20,7 @@ export default function ShopCard(props: IProps) {
     <>
       <div className="relative rounded overflow-hidden shadow-md">
           <div className="px-6 py-4">
-            <button className="font-bold text-xl mb-1 text-left block hover:underline" onClick={() => setIsOpen(true)}>{props.shop.name}</button>
+            <button className="font-medium text-xl mb-1 text-left block hover:underline" onClick={() => setIsOpen(true)}>{props.shop.name}</button>
             <button className="w-fit mb-1 text-left text-gray-700 border border-transparent hover:border-black hover:border-dashed hover:cursor-pointer" onClick={() => props.onShopClick(props.shop.neighborhood)}>{props.shop.neighborhood}</button>
             <address className="text-gray-700">{props.shop.address}</address>
           </div>
