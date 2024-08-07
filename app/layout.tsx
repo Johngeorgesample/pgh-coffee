6690b72a859cbff1555809f3ae0057a9bab56c31
John-George Sample <johngeorgesample@gmail.com>
2024-08-07 06:09:20 -0400

    use taggedEvents && outboundClicks

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <PlausibleProvider domain="pgh.coffee" />
+        <PlausibleProvider
+          domain="pgh.coffee"
+          taggedEvents
+          trackOutboundLinks
+        />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
