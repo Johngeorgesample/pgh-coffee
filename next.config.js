b4aa6159007ceabfbaae8bfeaa17b39d9b00bc0d
John-George Sample <johngeorgesample@gmail.com>
2024-08-07 05:42:31 -0400

    linting

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 const { withPlausibleProxy } = require('next-plausible')
 
 
-module.exports = withPlausibleProxy()({
-  // ...your next js config, if any
-  // Important! it is mandatory to pass a config object, even if empty
-})
+module.exports = withPlausibleProxy()({ })
