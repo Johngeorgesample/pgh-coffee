a7590583dab4a50fddc5d3b9ff072270bdcbb079
John-George Sample <johngeorgesample@gmail.com>
2024-08-07 05:39:57 -0400

    proxy plausible scripts

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const { withPlausibleProxy } = require('next-plausible')
 
-module.exports = nextConfig
+
+module.exports = withPlausibleProxy()({
+  // ...your next js config, if any
+  // Important! it is mandatory to pass a config object, even if empty
+})
