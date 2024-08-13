23ee4b75b823318541bd32a3d87ec1425cd7c961
John-George Sample <johngeorgesample@gmail.com>
2024-08-13 08:35:39 -0400

    Ensure we have a filter before calling methods on it, split header into new component

---
+++ app/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <div className="flex flex-col items-center mt-2">
+      <h1 className="text-4xl pb-6">pgh.coffee</h1>
+      <h3 className="text-center">A guide to every coffee shop in Pittsburgh, PA</h3>
+      <hr className="my-4 w-1/2 m-auto" />
+    </div>
+  )
+}
