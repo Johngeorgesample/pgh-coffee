a591b9b50b29cef69b3924642edc8fe5a096850a
John-George Sample <Johngeorgesample@gmail.com>
2024-08-13 08:40:17 -0400

    Ensure we have a filter before calling methods on it, split header into new component (#9)

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,14 @@
 import { useState, useMemo } from 'react'
 import ShopCard from '@/app/components/ShopCard'
 import CoffeeShops from '@/data/coffee_shops.json'
+import Header from '@/app/components/Header'
 import Footer from '@/app/components/Footer'
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
 
 export default function Home({ searchParams }: { searchParams: any }) {
   const [filter, setFilter] = useState('')
 
-  const handleQueryString = useMemo(() => {
+  useMemo(() => {
     if (typeof window !== 'undefined') {
       const params = new URLSearchParams(window.location.search)
       const myParam = params.get('neighborhood')
@@ -42,20 +43,18 @@ export default function Home({ searchParams }: { searchParams: any }) {
   }
 
   const meetsFilterCriteria = (shop: any) => {
-    const shopCardText = `${shop.neighborhood.toLowerCase()} ${shop.name.toLowerCase()}`
-    return shopCardText.includes(filter.toLowerCase())
+    if (filter) {
+      const shopCardText = `${shop.neighborhood.toLowerCase()} ${shop.name.toLowerCase()}`
+      return shopCardText.includes(filter.toLowerCase())
+    }
   }
 
   return (
     <>
       <main className="flex min-h-[calc(100vh-72px)] flex-col items-center">
         <div className="flex justify-center items-center flex-col absolute top-0 h-56">
-          <div className="flex flex-col items-center mt-2">
-            <h1 className="text-4xl pb-6">pgh.coffee</h1>
-            <h3 className="text-center">A guide to every coffee shop in Pittsburgh, PA</h3>
-            <hr className="my-4 w-1/2 m-auto" />
-          </div>
 
+          <Header />
           <div className="mb-2 border rounded-lg px-2 w-64 flex items-center justify-between">
             <span className="inline text-gray-500" aria-hidden="true">
               <MagnifyingGlassIcon className="h-4 w-4" />
@@ -74,7 +73,7 @@ export default function Home({ searchParams }: { searchParams: any }) {
 
         <div className="grid grid-cols-3 gap-4 px-4 mt-56">
           {coffeeShops.map(shop => {
-            if (meetsFilterCriteria(shop)) {
+            if (meetsFilterCriteria(shop) || !filter) {
               return <ShopCard key={shop.address} onShopClick={(shopName: any) => setFilter(shopName)} shop={shop} />
             }
           })}
