1525872dbe45d51c55b2828d5f99716526253b79
John-George Sample <johngeorgesample@gmail.com>
2024-08-19 12:05:50 -0400

    start to remove `value` - `photo` lives one-level higher

--- types/shop-types.ts
+++ types/shop-types.ts
@@ -4,6 +4,7 @@ export interface TShop {
   name: string
   neighborhood: string // @TODO make `TNeighborhood` work
   address: string
+  photo?: string
   website: string // @TODO make `URL` work
   value?: any
 }
