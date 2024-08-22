a5e0f0d5c5a5370ee23871950867ad7199466cc3
John-George Sample <johngeorgesample@gmail.com>
2024-08-22 13:33:03 -0400

    center text in header

--- app/components/Header2.tsx
+++ app/components/Header2.tsx
@@ -3,7 +3,7 @@ export default function Header2() {
     <div className="bg-yellow-300 flex items-center py-2 px-2">
       <a className="flex flex-1 gap-3" href="/map">
         <img className="h-12" src="https://upload.wikimedia.org/wikipedia/commons/1/1a/Keystone_state_symbol_Pennsylvania.svg" alt="" />
-        <h1 className="text-4xl">pgh.coffee</h1>
+        <h1 className="flex items-center text-4xl">pgh.coffee</h1>
       </a>
       <a href="/about">About</a>
     </div>
