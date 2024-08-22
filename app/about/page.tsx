33a9626f75ec91fdc2c368c6e0e5b73ad744368c
John-George Sample <johngeorgesample@gmail.com>
2024-08-22 08:56:38 -0400

    use lorem ipsum for about copy

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,12 +5,24 @@ export default function About() {
       <Header2 />
       <div className="max-w-4xl mx-auto px-6 md:px-8">
         <p className="mb-5 mt-4">
-          Hi, I'm John-George — a software engineer in Pittsburgh, PA. Currently, I'm a senior front-end engineer at{" "}
-
-          where we're helping students with their college search.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac tincidunt ante. Cras a dui lacinia, suscipit
+          ligula nec, porta leo. Suspendisse pharetra lacinia enim, eget gravida eros aliquet id. Suspendisse lobortis
+          vitae ante volutpat imperdiet. Sed molestie rutrum magna, at rutrum magna blandit sed. Nullam odio orci,
+          dictum nec magna id, faucibus interdum purus. Nullam nec euismod augue, ut venenatis tortor. Quisque quis
+          ipsum vel augue condimentum tincidunt ut id felis. Aenean egestas fringilla ipsum ut sodales. Etiam id
+          sollicitudin ipsum. Morbi sit amet metus vestibulum odio ullamcorper vulputate id eget mauris. Vivamus vitae
+          magna eget ex vestibulum interdum. In convallis a dolor sit amet congue. Pellentesque tincidunt dignissim
+          justo.
         </p>
         <p className="mb-5">
-          Outside of work, you can find me chasing the perfect cup of coffee at home and coffee shops in the city. My favorite drink is a pour over made with washed Ethiopian beans.
+          Nam finibus placerat velit, quis pretium mauris scelerisque quis. Nam vel eros sit amet sem dapibus auctor ac
+          sit amet quam. Nullam tellus massa, lacinia ut ultrices ut, facilisis id sapien. Curabitur at dignissim urna.
+          Etiam ac leo sed ex consectetur blandit in a diam. Pellentesque pulvinar lorem vel consequat imperdiet. Nullam
+          a velit et ex tristique rutrum a ac diam. Morbi sit amet ultricies risus. Aliquam erat volutpat. Pellentesque
+          commodo elit efficitur arcu malesuada molestie at id neque. Praesent ullamcorper magna egestas turpis
+          consequat commodo. Ut ornare risus non velit hendrerit faucibus. Integer dignissim, neque quis tincidunt
+          laoreet, dolor tellus scelerisque lectus, et condimentum metus odio sit amet lorem. Vestibulum scelerisque
+          magna quis neque lobortis interdum. Sed erat ipsum, accumsan rhoncus vehicula a, suscipit non urna.
         </p>
       </div>
     </>
