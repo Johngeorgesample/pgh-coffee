e6b22392e631ce09fc5c622b217fc9dd68bee7e4
John-George Sample <johngeorgesample@gmail.com>
2024-08-25 16:12:31 -0400

    start fixing types

--- types/shop-types.ts
+++ types/shop-types.ts
@@ -1,10 +1,19 @@
 import { TNeighborhood } from './neighborhood-types'
 
+export interface TShopProperties {
+  }
+
 export interface TShop {
-  name: string
-  neighborhood: string // @TODO make `TNeighborhood` work
-  address: string
-  photo?: string
-  website: string // @TODO make `URL` work
-  value?: any
+  type: string,
+  properties: {
+    name: string
+    neighborhood: string // @TODO make `TNeighborhood` work
+    address: string
+    photo?: string
+    website: string // @TODO make `URL` work
+  }
+  geometry: {
+    type: string,
+    photo: [number, number]
+  }
 }
