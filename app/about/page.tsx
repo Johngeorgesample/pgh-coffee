66cf0d798212f9d7cf2eb19447aefe9b3a14b1d3
John-George Sample <johngeorgesample@gmail.com>
2024-08-26 08:24:47 -0400

    semantics, add link to FAQ

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,13 +7,14 @@ export default function About() {
     <>
       <Nav />
       <div className="max-w-4xl mx-auto px-6 md:px-8 mt-16">
+        <h1 className="text-2xl text-bold mb-2">About</h1>
         <p className="mb-5">
         This project&apos;s first iteration was a spreadsheet. I tracked what coffee shops I had been to, where I still needed to visit, and general thoughts on my experience. Then I had the thought every developer has had - &quot;what if I turn the niche thing I care about into a side project?&quot; This site is the result of that thought.
         </p>
       </div>
 
       <div className="max-w-4xl mx-auto px-6 md:px-8">
-        <h3>FAQ</h3>
+        <h3 className="text-xl mb-2">FAQ</h3>
 
         <QAndA
           question="I&apos;d like to help! How can I contribute?"
@@ -21,7 +22,7 @@ export default function About() {
         />
 
         <QAndA
-          question="I found something wrong"
+          question="I found something wrong."
           answer="That&apos;s not a question, but pull requests are welcome."
         />
 
@@ -35,13 +36,8 @@ export default function About() {
           answer="Every photo has been taken by me (so far) to ensure there isn&apos;t copyright infringement."
         />
 
-
-        {/*<p>You can buy me a coffee if you see me around Pittsburgh. If you&apos;re not local, I also acccept digital coffee! <a className="underline text-blue-700" href="https://buymeacoffee.com/johngeorgesample">https://buymeacoffee.com/johngeorgesample</a></p>*/}
-
-        <QAndA
-          question="How can I support your work?"
-          answer=""
-        />
+      <p className="font-bold">How can I support your work?</p>
+      <p>You can buy me a cortado if you see me around Pittsburgh. Otherwise, you can buy me a digital coffee! <a className="underline text-blue-700" href="https://buymeacoffee.com/johngeorgesample">https://buymeacoffee.com/johngeorgesample</a></p>
 
       </div>
     </>
