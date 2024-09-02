a2bfa5e3b491797da417bba22f7f5538996cb15a
John-George Sample <johngeorgesample@gmail.com>
2024-09-02 16:09:58 -0400

    relative spacing for FAB

--- app/page.tsx
+++ app/page.tsx
@@ -126,7 +126,7 @@ export default function Mappy() {
           </Source>
         </Map>
         <button
-          className="absolute bottom-24 right-12 bg-yellow-300 hover:bg-yellow-400 rounded-full h-16 w-16 flex justify-center items-center"
+          className="absolute bottom-[10%] right-[5%] bg-yellow-300 hover:bg-yellow-400 rounded-full h-16 w-16 flex justify-center items-center"
           onClick={handleSearchClick}
         >
           <MagnifyingGlassIcon className="h-8 w-8" />
