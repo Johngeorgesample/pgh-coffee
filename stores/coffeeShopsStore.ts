eadfb28a317aba92b210d05a70b61aff971081ef
John-George Sample <johngeorgesample@gmail.com>
2024-09-19 08:12:05 -0400

    create coffeeShopsStore

---
+++ stores/coffeeShopsStore.ts
@@ -0,0 +1,32 @@
+import { create } from 'zustand'
+import { devtools } from 'zustand/middleware'
+import { TShop } from '@/types/shop-types'
+
+interface CoffeeShopsState {
+  coffeeShops: any
+  fetchCoffeeShops: () => Promise<void>
+  setCoffeeShops: (data: TShop[]) => void
+}
+
+const useCoffeeShopsStore = create<CoffeeShopsState>()(
+  devtools(
+    (set) => ({
+      coffeeShops: [],
+
+      setCoffeeShops: (data: TShop[]) => set({ coffeeShops: data }),
+
+      fetchCoffeeShops: async () => {
+        try {
+          const response = await fetch('/api/shops/geojson')
+          const data = await response.json()
+          set({ coffeeShops: data })
+        } catch (error) {
+          console.error('Error fetching coffee shops:', error)
+        }
+      }
+    }),
+    { name: 'ShopsStore' }
+  )
+)
+
+export default useCoffeeShopsStore
