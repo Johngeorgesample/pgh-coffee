880b210ad7e61fdf1f1fa5eb8521fe8659541b65
John-George Sample <johngeorgesample@gmail.com>
2024-09-22 14:25:09 -0400

    rm file upload

--- app/submit-a-shop/page.tsx
+++ app/submit-a-shop/page.tsx
@@ -97,28 +97,6 @@ export default function SubmitAShop() {
                   />
                 </div>
               </div>
-
-              <div className="col-span-full">
-                <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">
-                  Cover photo
-                </label>
-                <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-                  <div className="text-center">
-                    <PhotoIcon aria-hidden="true" className="mx-auto h-12 w-12 text-gray-300" />
-                    <div className="mt-4 flex text-sm leading-6 text-gray-600">
-                      <label
-                        htmlFor="file-upload"
-                        className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
-                      >
-                        <span>Upload a file</span>
-                        <input id="file-upload" name="file-upload" type="file" className="sr-only" />
-                      </label>
-                      <p className="pl-1">or drag and drop</p>
-                    </div>
-                    <p className="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
-                  </div>
-                </div>
-              </div>
             </div>
 
             <div className="sm:col-span-4">
