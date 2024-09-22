48ddba3b73550c2f8118fef9362793921764501a
John-George Sample <johngeorgesample@gmail.com>
2024-09-22 14:36:32 -0400

    rm email field

--- app/submit-a-shop/page.tsx
+++ app/submit-a-shop/page.tsx
@@ -98,22 +98,6 @@ export default function SubmitAShop() {
                 </div>
               </div>
             </div>
-
-            <div className="sm:col-span-4">
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                Email address
-              </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <p className="mt-3 text-sm leading-6 text-gray-600">If you want to be informed when your update is added</p>
           </div>
         </div>
 
