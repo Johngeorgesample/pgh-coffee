762a4b424e05d117c1d98da75c0075dfe2e9fa3d
John-George Sample <Johngeorgesample@gmail.com>
2024-10-04 08:29:25 -0400

    add og and twitter images (#68)

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,24 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'PGH Coffee',
   description: 'Coffee shops in Pittsburgh, PA',
+  openGraph: {
+    title: 'pgh.coffee',
+    description: 'A guide to coffee in Pittsburgh, PA',
+    images: [
+      {
+        url: 'https://uljutxoijtvtcxvatqso.supabase.co/storage/v1/object/public/pgh-coffee-misc/opengraph-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'A guide to coffee in Pittsburgh, PA',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'pgh.coffee',
+    description: 'A guide to coffee in Pittsburgh, PA',
+    images: ['https://uljutxoijtvtcxvatqso.supabase.co/storage/v1/object/public/pgh-coffee-misc/twitter-image.png'],
+  },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
