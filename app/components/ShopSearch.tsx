a49433d08adcf0f76712da983fe2dac43e84a0f8
John-George Sample <johngeorgesample@gmail.com>
2024-10-08 15:44:32 -0400

    use ShopList in ShopSearch

--- app/components/ShopSearch.tsx
+++ app/components/ShopSearch.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState, useRef } from 'react'
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
 import { TShop } from '@/types/shop-types'
 import useShopsStore from '@/stores/coffeeShopsStore'
+import ShopList from '@/app/components/ShopList'
 
 interface IProps {
   handleResultClick: (shop: TShop) => void
@@ -11,16 +12,6 @@ export default function ShopSearch(props: IProps) {
   const { coffeeShops } = useShopsStore()
   const inputRef = useRef<HTMLInputElement>(null)
   let [filter, setFilter] = useState('')
-
-  const meetsFilterCriteria = (shop: TShop) => {
-    if (filter) {
-      const shopCardText = `${shop.properties.neighborhood.toLowerCase()} ${shop.properties.name
-        .toLowerCase()
-        .normalize('NFD')
-        .replace(/[\u0300-\u036f]/g, '')}`
-      return shopCardText.includes(filter.toLowerCase())
-    }
-  }
 
   const handleFilterClear = () => {
     setFilter('')
@@ -64,33 +55,12 @@ export default function ShopSearch(props: IProps) {
         </div>
       </div>
 
-      <ul className="relative mt-6 flex-1 px-4 sm:px-6">
-        {coffeeShops.features.map((shop: TShop) => {
-          if (meetsFilterCriteria(shop) || !filter) {
-            return (
-              <li
-                key={shop.properties.name + shop.properties.address}
-                className="relative mb-4 rounded overflow-hidden shadow-md hover:cursor-pointer"
-                onClick={() => handleCardClick(shop)}
-                onKeyPress={event => handleKeyPress(event, shop)}
-                tabIndex={0}
-                role="button"
-              >
-                <div
-                  className="h-36 relative bg-yellow-200 bg-cover bg-center"
-                  style={shop.properties.photo ? { backgroundImage: `url('${shop.properties.photo}')` } : {}}
-                />
-                <div className="px-6 py-2">
-                  <p className="font-medium text-xl text-left block">{shop.properties.name}</p>
-                  <p className="w-fit mb-1 text-left text-gray-700 border border-transparent">
-                    {shop.properties.neighborhood}
-                  </p>
-                </div>
-              </li>
-            )
-          }
-        })}
-      </ul>
+      <ShopList
+        coffeeShops={coffeeShops.features}
+        filter={filter}
+        handleCardClick={handleCardClick}
+        handleKeyPress={handleKeyPress}
+      />
     </div>
   )
 }
