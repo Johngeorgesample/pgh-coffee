a3050d5927d66445f3525689cda83cee91942f14
John-George Sample <Johngeorgesample@gmail.com>
2024-12-22 16:09:26 -0500

    PWA (#92)

---
+++ app/manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'pgh.coffee',
+    short_name: 'pgh.coffee',
+    description: 'A guide to coffee in Pittsburgh, PA',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#FDE047',
+    theme_color: '#000000',
+    icons: [
+      {
+        src: '/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  }
+}
