b71562968345bb58d593dd58a447596a9752ca0f
John-George Sample <johngeorgesample@gmail.com>
2024-12-23 20:52:01 -0500

    wip

--- app/components/PanelContent.tsx
+++ app/components/PanelContent.tsx
@@ -14,8 +14,17 @@ interface IProps {
 export default function PanelContent(props: IProps) {
   return (
     <>
-      <div className="relative mt-4 mb-3 flex-1 text-2xl px-4 sm:px-6">
+      {/*
+      <div className="flex gap-2 items-center align-center mt-4 mb-3 flex-1 text-2xl px-4 sm:px-6">
         <p>{props.shop.properties.name}</p>
+        <p>•</p>
+        <p className="">{props.shop.properties.neighborhood}</p>
+      </div>
+      */}
+
+      <div className="flex flex-col gap-1 align-center mt-4 mb-3 flex-1 text-2xl px-4 sm:px-6">
+        <p className="font-medium">{props.shop.properties.name}</p>
+        <p className="text-lg">{props.shop.properties.neighborhood}</p>
       </div>
       <hr />
       <div className="relative mt-2 flex-1 px-4 sm:px-6">
@@ -66,27 +75,8 @@ export default function PanelContent(props: IProps) {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <address className="text-sm text-gray-900">{props.shop.properties.address}</address>
+            <address className="text-sm text-gray-900 hover:underline">{props.shop.properties.address}</address>
           </a>
-        </div>
-
-        <div className="flex mt-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-4 mr-2"
-          >
-            <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
-            />
-          </svg>
-          <p className="text-sm text-gray-900">{props.shop.properties.neighborhood}</p>
         </div>
       </div>
       <NearbyShops shop={props.shop} handleClick={props.handleNearbyShopClick} />
