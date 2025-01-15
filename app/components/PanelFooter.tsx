855b4b6e70c675a883f28e064867844ae7026bd9
John-George Sample <johngeorgesample@gmail.com>
2025-01-15 08:54:51 -0500

    bring back centered text

--- app/components/PanelFooter.tsx
+++ app/components/PanelFooter.tsx
@@ -12,7 +12,7 @@ export default function PanelFooter(props: IProps) {
   return (
     <section className="flex flex-col items-center">
       <hr className="my-4 w-1/2 m-auto" />
-      <div className="text-sm mb-2 flex flex-col flex-1">
+      <div className="text-sm mb-2 flex flex-col flex-1 items-center">
         <p className="text-gray-500">Is this information incorrect?</p>
         <p className="text-gray-500">
           <a aria-label="File an issue on GitHub" className="italic underline" href={githubURL}>
