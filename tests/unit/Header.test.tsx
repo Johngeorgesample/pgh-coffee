f3e7394daa4df160885629d679b1a6f9811f6e1a
John-George Sample <johngeorgesample@gmail.com>
2025-01-27 07:28:27 -0500

    add vitest, sample test for Header

---
+++ tests/unit/Header.test.tsx
@@ -0,0 +1,11 @@
+import { describe, it, expect } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import Header from '../../app/components/Header'
+
+describe('Header', () => {
+  it('renders correctly with all expected elements', () => {
+    render(<Header />)
+    expect(screen.getByText('pgh.coffee')).toBeInTheDocument()
+    expect(screen.getByText('A guide to every coffee shop in Pittsburgh, PA')).toBeInTheDocument()
+  })
+})
