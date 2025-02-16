3cf68286caaea37aea5324c3da20e43f2e9cc0bd
John-George Sample <johngeorgesample@gmail.com>
2025-02-15 20:47:09 -0500

    snip botched test

--- tests/unit/PanelHeader.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, test, expect, beforeEach, vi } from 'vitest'
-import { render, screen, fireEvent } from '@testing-library/react'
-import PanelHeader from '@/app/components/PanelHeader
-
-describe('ShopCard', () => {
-  test('foo', => {
-  })
-})
