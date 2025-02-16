8527300ffc59cce678b1cefe21ce106167dd6fd9
John-George Sample <johngeorgesample@gmail.com>
2025-02-15 20:46:07 -0500

    snip unused component

--- app/components/NeighborhoodBanner.tsx
+++
@@ -1,7 +0,0 @@
-interface IProps {
-  neighborhood: string
-}
-
-export default function NeighborhoodBanner(props: IProps) {
-  return <div className="w-full bg-yellow-200 px-4 py-2 my-4">{props.neighborhood}</div>
-}
