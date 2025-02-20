774b2ee14d2f3c09af9dcd68f4129f6c66a34538
John-George Sample <johngeorgesample@gmail.com>
2025-02-20 18:57:32 -0500

    add maskable icon

--- app/manifest.ts
+++ app/manifest.ts
@@ -20,6 +20,12 @@ export default function manifest(): MetadataRoute.Manifest {
         sizes: '512x512',
         type: 'image/png',
       },
+      {
+        src: '/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
     ],
   }
 }
