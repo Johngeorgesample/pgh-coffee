fa4ec67de3d429c4679a258cc44336d69ee98570
John-George Sample <johngeorgesample@gmail.com>
2025-07-13 21:39:52 -0400

    split mobile and desktop panels

--- app/components/ShopPanel.tsx
+++ app/components/ShopPanel.tsx
@@ -31,21 +31,39 @@ export default function ShopPanel(props: IProps) {
         <div className="fixed" />
         <div className="fixed overflow-hidden">
           <div className="absolute overflow-hidden">
-            <div className="w-full bottom-0 h-1/2 pointer-events-none fixed lg:w-fit lg:h-[calc(100%-4rem-3.5rem)] lg:inset-y-0 lg:top-16 lg:right-0 flex max-w-full lg:pl-10">
-              <Transition.Child
-                as={Fragment}
-                enter="transform transition ease-in-out duration-500 sm:duration-700"
-                enterFrom={isLargeScreen ? 'translate-x-full' : 'translate-y-full'}
-                enterTo={isLargeScreen ? 'translate-x-0' : 'translate-y-0'}
-                leave="transform transition ease-in-out duration-500 sm:duration-700"
-                leaveFrom={isLargeScreen ? 'translate-x-0' : 'translate-y-0'}
-                leaveTo={isLargeScreen ? 'translate-x-full' : 'translate-y-full'}
-              >
-                <div className="bg-neutral-50 overflow-y-auto pointer-events-auto w-screen lg:max-w-xl">
-                  {props.children}
-                </div>
-              </Transition.Child>
-            </div>
+            {isLargeScreen ? (
+              <div className="w-full bottom-0 h-1/2 pointer-events-none fixed lg:w-fit lg:h-[calc(100%-4rem-3.5rem)] lg:inset-y-0 lg:top-16 lg:right-0 flex max-w-full lg:pl-10">
+                <Transition.Child
+                  as={Fragment}
+                  enter="transform transition ease-in-out duration-500 sm:duration-700"
+                  enterFrom={isLargeScreen ? 'translate-x-full' : 'translate-y-full'}
+                  enterTo={isLargeScreen ? 'translate-x-0' : 'translate-y-0'}
+                  leave="transform transition ease-in-out duration-500 sm:duration-700"
+                  leaveFrom={isLargeScreen ? 'translate-x-0' : 'translate-y-0'}
+                  leaveTo={isLargeScreen ? 'translate-x-full' : 'translate-y-full'}
+                >
+                  <div className="bg-neutral-50 overflow-y-auto pointer-events-auto w-screen lg:max-w-xl">
+                    {props.children}
+                  </div>
+                </Transition.Child>
+              </div>
+            ) : (
+              <div className="w-full bottom-0 h-[90%] pointer-events-none fixed flex">
+                <Transition.Child
+                  as={Fragment}
+                  enter="transform transition ease-in-out duration-500 sm:duration-700"
+                  enterFrom={'translate-y-full'}
+                  enterTo={'translate-y-0'}
+                  leave="transform transition ease-in-out duration-500 sm:duration-700"
+                  leaveFrom={'translate-y-0'}
+                  leaveTo={'translate-y-full'}
+                >
+                  <div className="bg-neutral-50 overflow-y-auto pointer-events-auto w-screen lg:max-w-xl">
+                    {props.children}
+                  </div>
+                </Transition.Child>
+              </div>
+            )}
           </div>
         </div>
       </Dialog>
