d90c28c51a24eb4eb52be13bbbbfab0354bab5ec
John-George Sample <johngeorgesample@gmail.com>
2025-07-28 19:35:19 -0400

    resize panel && map

--- app/components/ShopPanel.tsx
+++ app/components/ShopPanel.tsx
@@ -11,10 +11,9 @@ interface IProps {
 export default function ShopPanel(props: IProps) {
   return (
     <div data-testid="shop-panel" className="relative z-10">
-      <div className="fixed" />
       <div className="fixed overflow-hidden">
         <div className="absolute overflow-hidden">
-          <div className="w-full bottom-0 h-1/2 pointer-events-none fixed lg:w-fit lg:h-[calc(100%-4rem-3.5rem)] lg:inset-y-0 lg:top-16 lg:right-0 flex max-w-full lg:pl-10">
+          <div className="w-full bottom-0 h-1/2 pointer-events-none fixed lg:w-1/3 lg:h-[calc(100%-4rem-3.5rem)] lg:inset-y-0 lg:top-16 lg:right-0 flex max-w-full">
             <div className="bg-neutral-50 overflow-y-auto pointer-events-auto w-screen lg:max-w-xl">
               {props.children}
             </div>
