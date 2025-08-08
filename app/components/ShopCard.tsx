e714de09d64cb94915ba466725b78823e5080e37
John-George Sample <Johngeorgesample@gmail.com>
2025-08-08 15:27:58 -0400

    Lazy load images from ShopCard (#139)
    * lazy load ShopCard images
    
    * add fallback if shop does not have image
    
    * update tests

--- app/components/ShopCard.tsx
+++ app/components/ShopCard.tsx
@@ -7,7 +7,6 @@ interface IProps {
   handleKeyPress: (event: React.KeyboardEvent<HTMLLIElement>, shop: TShop) => any
   shop: TShop
   units?: TUnits
-
 }
 
 export const roundDistance = ({ units, distance }: { units: string; distance: number }) => {
@@ -29,10 +28,16 @@ export default function ShopCard(props: IProps) {
       tabIndex={0}
       role="button"
     >
-      <div
-        className="h-36 relative bg-yellow-200 bg-cover bg-center"
-        style={props.shop.properties.photo ? { backgroundImage: `url('${props.shop.properties.photo}')` } : {}}
-      />
+      {props.shop?.properties?.photo ? (
+        <img
+          className="h-36 w-full relative object-cover object-center"
+          decoding="async"
+          loading="lazy"
+          src={props.shop.properties.photo}
+        />
+      ) : (
+        <div className="h-36 relative bg-yellow-200 bg-cover bg-center" />
+      )}
       <div className="px-6 py-2">
         <p className="font-medium text-xl text-left block">{props.shop.properties.name}</p>
         <p className="w-fit mb-1 text-left text-gray-700 border border-transparent">
