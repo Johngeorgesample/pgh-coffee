54971daacb3ffb8788a9d6a40a0b0f389289f4a1
John-George Sample <johngeorgesample@gmail.com>
2025-08-21 19:01:04 -0400

    update type

--- stores/panelStore.ts
+++ stores/panelStore.ts
@@ -2,7 +2,7 @@ import { create } from 'zustand'
 import { devtools } from 'zustand/middleware'
 import { ReactNode } from 'react'
 
-type PanelMode = 'explore' | 'search' | 'shop' | 'list' | 'news'
+type PanelMode = 'explore' | 'search' | 'shop' | 'list' | 'news' | 'events'
 
 type PanelEntry = {
   mode: PanelMode
