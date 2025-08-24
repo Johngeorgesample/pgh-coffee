c74a31f203e3576f08d6fb279558fcd6b0821c23
John-George Sample <johngeorgesample@gmail.com>
2025-08-24 13:54:08 -0400

    clicking shop triggers silk sheet

--- app/components/Panel.tsx
+++ app/components/Panel.tsx
@@ -1,7 +1,10 @@
 'use client'
 
+import { useState, useEffect } from 'react'
 import { Sheet } from '@silk-hq/components'
 import { TShop } from '@/types/shop-types'
+
+import useShopStore from '@/stores/coffeeShopsStore'
 import SearchBar from './SearchBar'
 
 interface IProps {
@@ -11,17 +14,39 @@ interface IProps {
 }
 
 export default function Panel(props: IProps) {
+  const currentShop = useShopStore(s => s.currentShop)
+  const [presented, setPresented] = useState(false)
+
+  useEffect(() => {
+    if (currentShop) {
+      setPresented(true)
+    } else {
+      setPresented(false)
+    }
+  }, [currentShop])
+
   return (
-    <Sheet.Root defaultPresented license="commercial">
+    <Sheet.Root
+      presented={presented}
+      onPresentedChange={next => {
+        setPresented(next)
+        if (!next) {
+          // optionally clear shop when sheet closes
+          useShopStore.getState().setCurrentShop({} as TShop)
+        }
+      }}
+      license="commercial"
+    >
       <Sheet.Portal>
-        <Sheet.View
-          className="BottomSheet-view"
-          nativeEdgeSwipePrevention
-        >
+        <Sheet.View className="BottomSheet-view" nativeEdgeSwipePrevention>
           <Sheet.Backdrop themeColorDimming="auto" />
           <Sheet.Content className="h-[90%] bg-neutral-50">
-            {/* cycles content -> full-height -> content ... */}
-            <Sheet.Handle action="dismiss" className="bg-gray-300">Drag to expand</Sheet.Handle>
+            <Sheet.Handle
+              action="dismiss"
+              className="block mx-auto focus:outline-none focus:ring-0 mt-2 mb-3 bg-gray-300"
+            >
+              Drag to expand
+            </Sheet.Handle>
 
             <SearchBar onClose={props.foo} />
             {props.children}
