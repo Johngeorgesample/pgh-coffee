179b0c5b5563769a9f03fca4ec48da2381ca2bd6
John-George Sample <johngeorgesample@gmail.com>
2025-08-24 21:14:32 -0400

    only use silk on mobile

--- app/components/Panel.tsx
+++ app/components/Panel.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-import { Sheet } from '@silk-hq/components'
+import { Sheet, useClientMediaQuery } from '@silk-hq/components'
 import { TShop } from '@/types/shop-types'
 
 import useShopStore from '@/stores/coffeeShopsStore'
@@ -16,6 +16,7 @@ interface IProps {
 export default function Panel(props: IProps) {
   const currentShop = useShopStore(s => s.currentShop)
   const [presented, setPresented] = useState(false)
+  const largeViewport = useClientMediaQuery('(min-width: 1024px)')
 
   useEffect(() => {
     if (currentShop && Object.keys(currentShop).length) {
@@ -25,7 +26,20 @@ export default function Panel(props: IProps) {
     }
   }, [currentShop])
 
-  return (
+  return largeViewport ? (
+    <div data-testid="shop-panel" className="relative z-10">
+      <div className="fixed overflow-hidden">
+        <div className="absolute overflow-hidden">
+          <div className="w-full bottom-0 h-full pointer-events-none fixed lg:w-1/3 lg:h-[calc(100%-4rem-3.5rem)] lg:inset-y-0 lg:top-16 lg:right-0 flex max-w-full">
+            <div className="bg-neutral-50 overflow-y-auto pointer-events-auto w-screen lg:max-w-4xl">
+              <SearchBar onClose={props.foo} />
+              {props.children}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  ) : (
     <Sheet.Root
       presented={presented}
       onPresentedChange={next => {
