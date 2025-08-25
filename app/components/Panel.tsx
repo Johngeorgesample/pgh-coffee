840a25664e4994f4e9321cf74683ba23574c914e
John-George Sample <johngeorgesample@gmail.com>
2025-08-24 21:33:54 -0400

    bring back margin around handle

--- app/components/Panel.tsx
+++ app/components/Panel.tsx
@@ -57,7 +57,7 @@ export default function Panel(props: IProps) {
           <Sheet.Content className="h-[90%] bg-neutral-50">
             <Sheet.Handle
               action="dismiss"
-              className="block mx-auto focus:outline-none focus:ring-0 bg-gray-300"
+              className="block mx-auto focus:outline-none focus:ring-0 mt-2 mb-3 bg-gray-300"
             >
               Drag to expand
             </Sheet.Handle>
