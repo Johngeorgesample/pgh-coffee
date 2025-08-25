fa51dc9b039dfe4deca18832c9fe1279b238112e
John-George Sample <johngeorgesample@gmail.com>
2025-08-24 21:44:33 -0400

    hide SearchBar on max detent

--- app/components/Panel.tsx
+++ app/components/Panel.tsx
@@ -1,9 +1,8 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { Sheet, useClientMediaQuery } from '@silk-hq/components'
 import { TShop } from '@/types/shop-types'
-
 import useShopStore from '@/stores/coffeeShopsStore'
 import SearchBar from './SearchBar'
 
@@ -16,43 +15,50 @@ interface IProps {
 export default function Panel(props: IProps) {
   const currentShop = useShopStore(s => s.currentShop)
   const [presented, setPresented] = useState(false)
+
+  // control detents
+  const detents = ['60vh'] as const
+  const lastDetentIndex = detents.length + 1 // 2 when you have ['60vh']
+  const [activeDetent, setActiveDetent] = useState<number | undefined>()
+  const isMax = (activeDetent ?? 1) === lastDetentIndex
+
   const largeViewport = useClientMediaQuery('(min-width: 1024px)')
 
   useEffect(() => {
-    if (currentShop && Object.keys(currentShop).length) {
-      setPresented(true)
-    } else {
-      setPresented(false)
-    }
+    setPresented(Boolean(currentShop && Object.keys(currentShop).length))
   }, [currentShop])
 
-  return largeViewport ? (
-    <div data-testid="shop-panel" className="relative z-10">
-      <div className="fixed overflow-hidden">
-        <div className="absolute overflow-hidden">
-          <div className="w-full bottom-0 h-full pointer-events-none fixed lg:w-1/3 lg:h-[calc(100%-4rem-3.5rem)] lg:inset-y-0 lg:top-16 lg:right-0 flex max-w-full">
-            <div className="bg-neutral-50 overflow-y-auto pointer-events-auto w-screen lg:max-w-4xl">
-              <SearchBar onClose={props.foo} />
-              {props.children}
+  if (largeViewport) {
+    return (
+      <div data-testid="shop-panel" className="relative z-10">
+        <div className="fixed overflow-hidden">
+          <div className="absolute overflow-hidden">
+            <div className="w-full bottom-0 h-full pointer-events-none fixed lg:w-1/3 lg:h-[calc(100%-4rem-3.5rem)] lg:inset-y-0 lg:top-16 lg:right-0 flex max-w-full">
+              <div className="bg-neutral-50 overflow-y-auto pointer-events-auto w-screen lg:max-w-4xl">
+                <SearchBar onClose={props.foo} />
+                {props.children}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  ) : (
+    )
+  }
+
+  return (
     <Sheet.Root
       presented={presented}
-      onPresentedChange={next => {
-        setPresented(next)
-        // if (!next) {
-        //   // optionally clear shop when sheet closes
-        //   useShopStore.getState().setCurrentShop({} as TShop)
-        // }
-      }}
+      onPresentedChange={setPresented}
+      activeDetent={activeDetent}
+      onActiveDetentChange={setActiveDetent}
       license="commercial"
     >
       <Sheet.Portal>
-        <Sheet.View className="BottomSheet-view" detents={['60vh']} nativeEdgeSwipePrevention>
+        <Sheet.View
+          className="BottomSheet-view"
+          detents={detents as unknown as string[]} // Silk typing allows string | string[]
+          nativeEdgeSwipePrevention
+        >
           <Sheet.Backdrop themeColorDimming="auto" />
           <Sheet.Content className="h-[90%] bg-neutral-50">
             <Sheet.Handle
@@ -62,8 +68,9 @@ export default function Panel(props: IProps) {
               Drag to expand
             </Sheet.Handle>
 
-            <SearchBar onClose={props.foo} />
+            {!isMax && <SearchBar onClose={props.foo} />}
             {props.children}
+
             <Sheet.BleedingBackground className="BottomSheet-bleedingBackground" />
           </Sheet.Content>
         </Sheet.View>
