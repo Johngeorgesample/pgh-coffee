2ca03d675d343a763304621ffc203185c0c27a73
John-George Sample <johngeorgesample@gmail.com>
2025-08-28 20:17:20 -0400

    clear search value when Explore mounts

--- app/components/ExploreContent.tsx
+++ app/components/ExploreContent.tsx
@@ -1,11 +1,18 @@
 'use client'
 
+import { useEffect } from 'react'
 import { ListChips } from './Explore/ListChips'
 import FeaturedShop from './Explore/FeaturedShop'
 import { EventsCTA } from './Explore/EventsCTA'
 import { CTAPhotoGrid } from './Explore/CTAPhotoGrid'
+import usePanelStore from '@/stores/panelStore'
 
 export const ExploreContent = () => {
+  const { setSearchValue } = usePanelStore()
+  useEffect(() => {
+    console.log('clearing')
+    setSearchValue('')
+  }, [])
   return (
     <div className="flex h-full overflow-y-auto flex-col sm:grid sm:grid-cols-2 gap-4 px-6 lg:px-4 lg:mt-16">
       <CTAPhotoGrid />
