b19bb827de680cf9c1b882cdc14febd441d504e5
John-George Sample <johngeorgesample@gmail.com>
2025-12-29 16:01:13 -0500

    rm

--- app/components/ShopSearch.tsx
+++ app/components/ShopSearch.tsx
@@ -1,5 +1,3 @@
-import { usePlausible } from 'next-plausible'
-import { TShop } from '@/types/shop-types'
 import useShopsStore from '@/stores/coffeeShopsStore'
 import usePanelStore from '@/stores/panelStore'
 import ShopList from '@/app/components/ShopList'
@@ -10,17 +8,6 @@ interface IProps {
 export default function ShopSearch(props: IProps) {
   const { allShops } = useShopsStore()
   const { searchValue } = usePanelStore()
-  const plausible = usePlausible()
-
-
-  // const handleCardClick = (shop: TShop) => {
-  //   plausible('ShopSearchClick', {
-  //     props: {
-  //       shopName: shop.properties.name,
-  //       neighborhood: shop.properties.neighborhood,
-  //     },
-  //   })
-  // }
 
   return (
     <div className="flex h-full flex-col overflow-y-auto px-4 sm:px-6">
