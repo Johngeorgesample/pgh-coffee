877d4f323135ee59d0ab63216b0b8f634547e3cf
John-George Sample <johngeorgesample@gmail.com>
2025-12-30 14:22:31 -0500

    render shops related to a company as ShopList

--- app/components/Company.tsx
+++ app/components/Company.tsx
@@ -1,6 +1,8 @@
 'use client'
+import ShopList from '@/app/components/ShopList'
 
 import { useState, useEffect } from 'react'
+import { formatDataToGeoJSON } from '../utils/utils'
 
 export const Company = ({ slug }: { slug: string }) => {
   const [company, setCompany] = useState<any>(null)
@@ -23,10 +25,22 @@ export const Company = ({ slug }: { slug: string }) => {
   if (loading) return <p>Loading...</p>
   if (!company) return <p>Company not found</p>
 
+  const shopsGeoJSON = formatDataToGeoJSON(company.shops)
+
   return (
-    <div>
+    <div className="px-6 lg:px-4 mt-24 lg:mt-16 flex flex-col">
       <h2>{company.name}</h2>
+      <p>{company.description}</p>
+      <a href={company.website} target="_blank">
+        {company.website}
+      </a>
+      <a href={`https://www.instagram.com/${company.instagram_handle}/`} target="_blank">
+        {company.instagram_handle}
+      </a>
+      <img src={company.logo} />
       <p>{company.shops?.length || 0} shops</p>
+
+      <ShopList coffeeShops={shopsGeoJSON.features} />
     </div>
   )
 }
