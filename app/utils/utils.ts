7cc7349a263ee2a58d62b630af51e7e6fa30bb15
John-George Sample <johngeorgesample@gmail.com>
2025-12-31 23:45:23 -0500

    add util for converting single instance of shop from DB to geojson feature

--- app/utils/utils.ts
+++ app/utils/utils.ts
@@ -28,6 +28,27 @@ export const formatDataToGeoJSON = (shops: DbShop[]): TFeatureCollection => {
   return myObj
 }
 
+export const formatDBShopAsFeature = (shop: DbShop): any => {
+  return (
+    {
+      type: 'Feature',
+      properties: {
+        name: shop.name,
+        company: shop.company,
+        neighborhood: shop.neighborhood,
+        website: shop.website,
+        address: shop.address,
+        photo: shop.photo ?? undefined,
+        uuid: shop.uuid
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [shop.longitude ?? 0, shop.latitude ?? 0],
+      }
+    }
+  )
+}
+
 export const parseYMDLocal = (ymd: string) => {
   const [y, m, d] = ymd.split('-').map(Number)
   return new Date(y, m - 1, d)
