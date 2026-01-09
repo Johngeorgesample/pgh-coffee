b115ba52e1d85f3927653e3d23e40f9439da81ab
John-George Sample <johngeorgesample@gmail.com>
2026-01-09 16:54:23 -0500

    n

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,29 @@
-'use client'
-
+import type { Metadata } from 'next'
 import HomeClient from './components/HomeClient'
+
+export async function generateMetadata({
+  searchParams,
+}: {
+  searchParams: Promise<{ shop?: string }>
+}): Promise<Metadata> {
+  const params = await searchParams
+  const shopParam = params.shop
+
+  if (shopParam) {
+    const ogUrl = `/api/og?shop=${encodeURIComponent(shopParam)}`
+    return {
+      openGraph: {
+        images: [ogUrl],
+      },
+      twitter: {
+        images: [ogUrl],
+      },
+    }
+  }
+
+  return {}
+}
+
 export default function Home() {
   return <HomeClient />
 }
