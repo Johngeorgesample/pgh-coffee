ffff8d303e10a586557d215c042234cb85ac7cfb
John-George Sample <johngeorgesample@gmail.com>
2026-01-09 20:45:32 -0500

    fetch events w/ roasters

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -6,21 +6,27 @@ const supabaseUrl = process.env.SUPABASE_URL as string
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY as string
 const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
-const fetchEvents = async (shopID?: string) => {
+const fetchEvents = async (shopID?: string, roasterID?: string) => {
   let query = supabase
     .from('events')
-    .select('*, shop:shop_id(*, company:company_id(*))')
+    .select('*, shop:shop_id(*, company:company_id(*)), roaster:roaster_id(*)')
     .order('event_date', { ascending: false })
 
   if (shopID) {
     query = query.eq('shop_id', shopID)
   }
 
+  if (roasterID) {
+    query = query.eq('roaster_id', roasterID)
+  }
+
   const { data, error } = await query
+
   if (error) {
     console.error('Error fetching events:', error.message)
     return null
   }
+
   return data
 }
 
@@ -28,8 +34,9 @@ const fetchEvents = async (shopID?: string) => {
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url)
   const shopID = searchParams.get('shop_id') ?? ''
+  const roasterID = searchParams.get('roaster_id') ?? ''
 
-  const events = await fetchEvents(shopID)
+  const events = await fetchEvents(shopID, roasterID)
 
   if (!events) {
     return NextResponse.json({ error: 'Error fetching events' }, { status: 500 })
