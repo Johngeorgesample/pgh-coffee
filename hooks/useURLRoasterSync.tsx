f67b7babb3ac3b35e62df5972c2fb0df6fafe118
John-George Sample <johngeorgesample@gmail.com>
2026-01-13 08:33:10 -0500

    move func inside useEffect

--- hooks/useURLRoasterSync.tsx
+++ hooks/useURLRoasterSync.tsx
@@ -5,29 +5,27 @@ import { RoasterDetails } from '@/app/components/RoasterDetails'
 export const useURLRoasterSync = () => {
   const { setPanelContent } = usePanelStore()
 
-  const fetchRoasterFromURL = async () => {
-    const params = new URLSearchParams(window.location.search)
-    const roasterSlug = params.get('roaster')
-
-    if (!roasterSlug) {
-      return
-    }
-
-    try {
-      const res = await fetch(`/api/roasters/${roasterSlug}`)
-      if (!res.ok) throw new Error('Roaster not found')
-      await res.json()
-      setPanelContent(<RoasterDetails slug={roasterSlug} />, 'roaster')
-    } catch (e) {
-      console.error(e)
-    }
-  }
-
   useEffect(() => {
+    const fetchRoasterFromURL = async () => {
+      const params = new URLSearchParams(window.location.search)
+      const roasterSlug = params.get('roaster')
+
+      if (!roasterSlug) {
+        return
+      }
+
+      try {
+        const res = await fetch(`/api/roasters/${roasterSlug}`)
+        if (!res.ok) throw new Error('Roaster not found')
+        await res.json()
+        setPanelContent(<RoasterDetails slug={roasterSlug} />, 'roaster')
+      } catch (e) {
+        console.error(e)
+      }
+    }
+
     fetchRoasterFromURL()
     window.addEventListener('popstate', fetchRoasterFromURL)
     return () => window.removeEventListener('popstate', fetchRoasterFromURL)
-    // only run on mount
-    // eslint-disable-next-line
-  }, [])
+  }, [setPanelContent])
 }
