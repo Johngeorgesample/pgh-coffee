811273d868f10513c081b10e57e57f21a7211ec5
John-George Sample <johngeorgesample@gmail.com>
2026-01-31 17:54:31 -0500

    split components

---
+++ app/components/SaveModalListItem.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { Bookmark } from 'lucide-react'
+
+interface SaveModalListItemProps {
+  name: string
+  count: string
+  id: string
+  isSelected: boolean
+  onClick: (id: string) => void
+}
+
+export default function SaveModalListItem({ name, count, id, isSelected, onClick }: SaveModalListItemProps) {
+  return (
+    <li
+      className={`flex gap-2 py-2 ${isSelected ? 'bg-red-100 ' : 'hover:bg-slate-100 '}`}
+      onClick={() => onClick(id)}
+    >
+      <div className="flex items-center">
+        <div className="bg-slate-200 p-2 rounded-lg">
+          <Bookmark />
+        </div>
+      </div>
+      <div className="flex flex-col">
+        <p>{name}</p>
+        <p className="text-slate-500 text-xs">{count} shops</p>
+      </div>
+    </li>
+  )
+}
