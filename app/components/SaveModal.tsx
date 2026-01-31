d242e9f9511a4491fa253cb00744befdfad7959e
John-George Sample <johngeorgesample@gmail.com>
2026-01-31 17:54:31 -0500

    split components

--- app/components/SaveModal.tsx
+++ app/components/SaveModal.tsx
@@ -1,7 +1,8 @@
 'use client'
+
 import { useState } from 'react'
 import { Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
-import { Bookmark } from 'lucide-react'
+import SaveModalListItem from './SaveModalListItem'
 
 interface Props {
   isOpen: boolean
@@ -9,31 +10,11 @@ interface Props {
   shopName: string
 }
 
-export default function IssueModal({ isOpen, onClose, shopName }: Props) {
+export default function SaveModal({ isOpen, onClose, shopName }: Props) {
   const [selectedLists, setSelectedLists] = useState<string[]>([])
 
   const handleClick = (id: string) => {
     setSelectedLists(prev => (prev.includes(id) ? prev.filter(item => item !== id) : [...prev, id]))
-  }
-
-  const ListItem = ({ name, count, id }: { name: string; count: string; id: string }) => {
-    const isSelected = selectedLists.includes(id)
-    return (
-      <li
-        className={`flex gap-2 py-2 ${isSelected ? 'bg-red-100 ' : 'hover:bg-slate-100 '}`}
-        onClick={() => handleClick(id)}
-      >
-        <div className="flex items-center">
-          <div className="bg-slate-200 p-2 rounded-lg">
-            <Bookmark />
-          </div>
-        </div>
-        <div className="flex flex-col">
-          <p>{name}</p>
-          <p className="text-slate-500 text-xs">{count} shops</p>
-        </div>
-      </li>
-    )
   }
 
   const data = [
@@ -43,6 +24,7 @@ export default function IssueModal({ isOpen, onClose, shopName }: Props) {
     { name: 'Bar', count: '3', id: '4' },
     { name: 'Baz', count: '3', id: '5' },
   ]
+
   return (
     <Dialog open={isOpen} onClose={onClose} className="relative z-50">
       <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
@@ -56,9 +38,16 @@ export default function IssueModal({ isOpen, onClose, shopName }: Props) {
           </DialogTitle>
 
           <ul className="py-2 max-h-[35dvh] overflow-scroll">
-            {data.map(list => {
-              return <ListItem key={list.id} name={list.name} count={list.count} id={list.id} />
-            })}
+            {data.map(list => (
+              <SaveModalListItem
+                key={list.id}
+                name={list.name}
+                count={list.count}
+                id={list.id}
+                isSelected={selectedLists.includes(list.id)}
+                onClick={handleClick}
+              />
+            ))}
           </ul>
           <div className="border-t -mx-6 px-6 pt-4">
             <div className="flex gap-2 border border-dashed p-2">
