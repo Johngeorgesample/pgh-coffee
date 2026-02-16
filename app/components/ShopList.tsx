b4e2821d088096988e4622591ac773ff7e77c1c1
John-George Sample <johngeorgesample@gmail.com>
2026-02-15 21:10:31 -0500

    remove filter fromo ShopList

--- app/components/ShopList.tsx
+++ app/components/ShopList.tsx
@@ -1,7 +1,6 @@
 import { TShop } from '@/types/shop-types'
 import { TUnits } from '@/types/unit-types'
 import ShopCard from '@/app/components/ShopCard'
-import { doesShopMatchFilter } from '@/app/utils/utils'
 
 interface IProps {
   coffeeShops: TShop[]
@@ -12,22 +11,18 @@ interface IProps {
 }
 
 export default function ShopList(props: IProps) {
-
   return (
     <ul className="relative mt-6 flex-1">
       {props.coffeeShops.map((shop: TShop, index) => {
-        if (doesShopMatchFilter(shop.properties.name, shop.properties.neighborhood, props.filter)) {
-          return (
-            <ShopCard
-              key={shop.properties.name + shop.properties.address}
-              distance={props.distances?.[index] != null ? String(props.distances[index]) : undefined}
-              hideShopName={props.hideShopNames}
-              shop={shop}
-              units={props.units}
-            />
-          )
-        }
-        return null
+        return (
+          <ShopCard
+            key={shop.properties.name + shop.properties.address}
+            distance={props.distances?.[index] != null ? String(props.distances[index]) : undefined}
+            hideShopName={props.hideShopNames}
+            shop={shop}
+            units={props.units}
+          />
+        )
       })}
     </ul>
   )
