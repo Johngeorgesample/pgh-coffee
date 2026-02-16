5aa6ff09a7285fb01aa1e132e2464db132577f36
John-George Sample <johngeorgesample@gmail.com>
2026-02-16 15:25:53 -0500

    add api-docs page

---
+++ app/api-docs/page.tsx
@@ -0,0 +1,8 @@
+'use client'
+
+import SwaggerUI from 'swagger-ui-react'
+import 'swagger-ui-react/swagger-ui.css'
+
+export default function ApiDocs() {
+  return <SwaggerUI url="/openapi.json" />
+}
