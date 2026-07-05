d3fa0263409272aa05bf3169f06fed352b53e0a7
John-George Sample <johngeorgesample@gmail.com>
2026-07-05 19:42:47 -0400

    Track ProfileView in analytics on public profile page

--- app/u/[id]/page.tsx
+++ app/u/[id]/page.tsx
@@ -4,6 +4,7 @@ import { User } from 'lucide-react'
 import VisitStats from '@/app/account/components/VisitStats'
 import { getPublicProfile } from '@/app/utils/profiles'
 import Passport from './Passport'
+import ProfileViewTracker from './ProfileViewTracker'
 
 type Props = {
   params: Promise<{ id: string }>
@@ -31,6 +32,7 @@ export default async function PublicProfilePage({ params }: Props) {
 
   return (
     <div className="min-h-[calc(100vh-4rem)] bg-gray-50">
+      <ProfileViewTracker profileId={id} />
       <div className="max-w-3xl mx-auto p-4 sm:p-6 md:p-8 space-y-6">
         <div className="bg-white rounded-2xl shadow-lg p-4 sm:p-6 md:p-8">
           <div className="flex items-center gap-4">
