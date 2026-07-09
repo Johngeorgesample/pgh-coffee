3399eaa22f189b753335e9c9d3f619fb0a68e29b
John-George Sample <Johngeorgesample@gmail.com>
2026-07-09 08:26:19 -0400

    Clarify verified badge tooltip (#335)
    The badge means the listing was claimed by someone at the business,
    not that pgh.coffee vouched for it.

--- app/components/VerifiedBadge.tsx
+++ app/components/VerifiedBadge.tsx
@@ -6,8 +6,8 @@ interface IProps {
 
 export default function VerifiedBadge({ className = '' }: IProps) {
   return (
-    <span title="Verified by pgh.coffee" className={`inline-flex shrink-0 ${className}`}>
-      <CheckBadgeIcon className="size-5 text-yellow-300 drop-shadow" />
+    <span title="Claimed by this business" className={`inline-flex shrink-0 ${className}`}>
+      <CheckBadgeIcon className="size-5 text-yellow-300" />
       <span className="sr-only">Verified</span>
     </span>
   )
